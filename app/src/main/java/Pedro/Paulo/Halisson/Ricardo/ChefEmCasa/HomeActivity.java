469b881fcf2890617b69f7b4087b842fc7b1f877
20211TIIMI0117 <pedroantoiocec@gmail.com>
2023-06-23 11:00:27 -0300

    daniel fodas

--- app/src/main/java/Pedro/Paulo/Halisson/Ricardo/ChefEmCasa/HomeActivity.java
+++ app/src/main/java/Pedro/Paulo/Halisson/Ricardo/ChefEmCasa/HomeActivity.java
@@ -3,6 +3,8 @@ package Pedro.Paulo.Halisson.Ricardo.ChefEmCasa;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -11,4 +13,11 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
     }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.home_toolbar_menu, menu);
+        return true;
+        }
 }
