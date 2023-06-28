0e92ddc9069e2dec3d27e0cf11d21a583ecd6bbf
20211TIIMI0117 <pedroantoniocec@gmail.com>
2023-06-28 10:49:30 -0300

    favorito feito

---
+++ app/src/main/java/Pedro/Paulo/Halisson/Ricardo/ChefEmCasa/MyRecipesFragment.java
@@ -0,0 +1,14 @@
+package Pedro.Paulo.Halisson.Ricardo.ChefEmCasa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MyRecipesFragment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_recipes_fragment);
+    }
+}
