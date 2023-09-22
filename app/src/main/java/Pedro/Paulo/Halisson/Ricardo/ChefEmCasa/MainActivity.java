5d6b009cb107ba4abb221989321b21134f723d9f
20211TIIMI0117 <pedroantoiocec@gmail.com>
2023-09-22 10:56:51 -0300

    USUARIO COMEÇADO, vsf HALLISON JULIO LOPES. SUCUMBA SEU MISERAVEL

--- app/src/main/java/Pedro/Paulo/Halisson/Ricardo/ChefEmCasa/MainActivity.java
+++ app/src/main/java/Pedro/Paulo/Halisson/Ricardo/ChefEmCasa/MainActivity.java
@@ -3,6 +3,8 @@ package Pedro.Paulo.Halisson.Ricardo.ChefEmCasa;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +13,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.home_toolbar_menu, menu);
+        return true;
+        }
 }
