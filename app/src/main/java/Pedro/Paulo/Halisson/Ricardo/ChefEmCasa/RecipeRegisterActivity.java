9f76869f4a4a1142a349c156448eda00e74021e5
20211TIIMI0117 <pedroantoiocec@gmail.com>
2023-10-06 10:59:11 -0300

    tem que criar a criacao de receitas e começar fazer as conexoes das telas conectar os fragmentos e comecar o banco de dados php

---
+++ app/src/main/java/Pedro/Paulo/Halisson/Ricardo/ChefEmCasa/RecipeRegisterActivity.java
@@ -0,0 +1,14 @@
+package Pedro.Paulo.Halisson.Ricardo.ChefEmCasa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RecipeRegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipe_register);
+    }
+}
