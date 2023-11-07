bb2b23f14ab056ce452b594e9c85dc16ad2bf853
Paulo Victor <paulovictoralves.contato@gmail.com>
2023-11-07 11:09:11 -0300

    Adicionamos o link do site, fizemos as final String do cadastro

--- app/src/main/java/Pedro/Paulo/Halisson/Ricardo/ChefEmCasa/Util/Config.java
+++ app/src/main/java/Pedro/Paulo/Halisson/Ricardo/ChefEmCasa/Util/Config.java
@@ -11,7 +11,7 @@ import android.content.SharedPreferences;
 public class Config {
 
     // endereço base do servidor web
-    public static String PRODUCTS_APP_URL = "https://productifes-dispmoveisbsi.b4a.run/";
+    public static String PRODUCTS_APP_URL = "https://chefemcasa-41d0oejn.b4a.run/movel/";
 
     /**
      * Salva o login no espaço reservado da app
